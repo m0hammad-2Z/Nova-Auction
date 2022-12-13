8f14b7349620121304e0079d118c017d46f6c63f
Alshami-exe <88931799+Alshami-exe@users.noreply.github.com>
2022-12-13 21:10:54 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -10,6 +10,38 @@ pass varchar(50),
 phonenumber varchar(20)
 );
 
+create table city(
+id int PRIMARY key AUTO_INCREMENT,
+city_name varchar(20)
+);
 
+create table car_info_makes(
+id int PRIMARY key AUTO_INCREMENT,
+makes_name varchar(20)
+);
 
+create table car_info_model(
+id int PRIMARY key AUTO_INCREMENT,
+model_name varchar(20)
+);
+
+create table car_info_type(
+id int PRIMARY key AUTO_INCREMENT,
+type_name varchar(20)
+);
+
+create table car_info(
+id int PRIMARY key AUTO_INCREMENT,
+year_of_make date,
+user_id int,
+makes int,
+model int,
+type int,
+city int,
+FOREIGN KEY (user_id) REFERENCES user_info(id),
+FOREIGN KEY (makes) REFERENCES car_info_makes(id),
+FOREIGN KEY (model) REFERENCES car_info_model(id),
+FOREIGN KEY (type) REFERENCES car_info_type(id),
+FOREIGN KEY (city) REFERENCES city(id)
+);
 
