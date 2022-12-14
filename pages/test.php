d30653af36ad81db4a98b718ea9d1c0db41ff244
Alshami-exe <88931799+Alshami-exe@users.noreply.github.com>
2022-12-14 19:10:33 +0300

    .

--- pages/test.php
+++ pages/test.php
@@ -1,16 +1,37 @@
 <?php
 // init PHP
 require "../lib.php"; 
-$res =Database("select makes_name , model_name from car_info_makes,car_info_model where makes = car_info_makes.id ",1);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form method="post" enctype="multipart/form-data">
+        <label for="file" >image<input name="img" type="file"></label>
+        <button type="submit" name="but">send</button>
+    </form>
+    <?php 
+        if(isset($_POST['but'])){
+            print_r( $_FILES['img']);
 
+
+        $filename = $_FILES["img"]["name"];
+        $tempname = $_FILES["img"]["tmp_name"];   
+        $folder = "../user_images/" . $filename;
+        move_uploaded_file($tempname, $folder);
+        }
+    ?>
+</body>
+</html>
 
 
 <script type="text/javascript">
-    
-    var jArray = <?php echo json_encode($res); ?>;
-    console.log(jArray);
- 
 
  </script>
