fae81d39a1839d2c6696c16ac6262cb340a28923
Alshami-exe <88931799+Alshami-exe@users.noreply.github.com>
2023-03-20 16:02:16 +0300

    j

--- pages/auction.php
+++ pages/auction.php
@@ -36,79 +36,122 @@
 
     
     <div class="main">
-        <div class="item">
-            <div class="main-pic">
-                <img id="blurred" src="https://picsum.photos/1001" alt="">
-                <img src="https://picsum.photos/1001" alt="">
+        
+            <div class="left-container">
+            <div class="item-pic container">
+                <div class="main-pic">
+                    <img id="blurred" src="https://picsum.photos/1001" alt="">
+                    <img src="https://picsum.photos/1001" alt="">
+                </div>
+
+                <div class="side-pics">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                    <img src="https://picsum.photos/100" alt="">
+                </div>
+
             </div>
-            <div class="side-pics">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
-                <img src="https://picsum.photos/100" alt="">
+                    <div class="info container">
+                        <div class="user-info">
+                        <img src="https://picsum.photos/100?b" alt="">
+                        <span><a href="#">mohammod mahmod</a></span>
+                        </div>
+                    </div>
+                    
             </div>
-            <div class="info">
-                <h1>hi</h1>
-                <h1>hi</h1>
-                <h1>hi</h1>
-                <h1>hi</h1>
-                <h1>hi</h1>
-                <h1>hi</h1>
-                <h1>hi</h1>
-                <h1>hi</h1>
-            </div>
-           
-        </div>
-        <div class="bid">
-            <div class="main-bid">
-            <input type="number">
-            <button class="button">place a bid</button>
+            <div class="right-container">
+
+            <div class="bid container">
+
+
+                <div class="main-bid">
+                    <h2>Place a Bid</h2>
+                    <h3>Last Bid :7500$</h3>
+                    <h3>Timer :2s</h3>
+                    <input type="number">
+                    <button class="button">place a bid</button>
             </div>
 
             <div class="last-bid">
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
+                <h2>Last bidders</h2>
+                <div class="last-bid-info">
+                    <p>fdsf</p>
+                    <p>fdsf</p>
+                    <p>fdsf</p>
+                    <p>fdsf</p>
+                    <p>fdsf</p>
+                    <p>fdsf</p>
+                    <p>fdsf</p>
+
+            
+                </div>
             </div>
         </div>
-        <div class="live-comment">
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
-                <h1>fdsf</h1>
+        
+    
+        <div class="live-comments container">
+                <h2>Live Comment</h2>
+               <div class="live-comment">
+                <p>hii</p>
+                <p>hidi</p>
+                <p>hwaii</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+                <p>hidwi</p>
+               </div>
+               <div>
+                    <input type="text">
+                    <button class="button">Send</button>
+               </div>
         </div>
     </div>
-
+    </div>
     <footer class="footer">
         <p>Copyright &copy; 2022 Nova Auction | Design By Humble Ghost Team</p>
     </footer>
