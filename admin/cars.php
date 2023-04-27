bb94aad038af23596a77227646d835040c1aa6c5
Alshami-exe <88931799+Alshami-exe@users.noreply.github.com>
2023-04-27 22:32:41 +0300

    .

--- admin/cars.php
+++ admin/cars.php
@@ -21,16 +21,16 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Admin Page</title>
-    <link rel="stylesheet" href="././users.css" />
-    <link rel="stylesheet" href="././index.css" />
+    <link rel="stylesheet" href="./users.css" />
+    <link rel="stylesheet" href="./index.css" />
   </head>
   <body>
     <header>
       <nav>
         <ul>
-          <li><a href="././index.php">Dashboard</a></li>
-          <li><a href="././users.php">Users</a></li>
-          <li><a href="././cars.php">Cars</a></li>
+          <li><a href="./index.php">Dashboard</a></li>
+          <li><a href="./users.php">Users</a></li>
+          <li><a href="./cars.php">Cars</a></li>
           <li><a href="#">Logout</a></li>
         </ul>
       </nav>
