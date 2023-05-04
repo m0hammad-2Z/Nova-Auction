0d84d41885d6e84d0a4b5ec6712b51902c72f683
Mohammad Zaro <imhamd33@gmail.com>
2023-05-04 23:09:33 +0300

    123

--- index.php
+++ index.php
@@ -61,7 +61,165 @@ require_once "./lib.php";
     </div>
     <footer class='footer'>
         <p>Copyright © 2022 Nova | Design By Humble Ghost Team</p>
-    </footer>
-</body>
+        
+    <?php
+         $carsRes =  Database('Select cars.id, cars.makes_name, cars.model_name, cars.color, items.price, cars.year_of_make from cars, items where items.car_id = cars.id', 1, MYSQLI_NUM);
+         $carsJson = json_encode($carsRes);
+         echo "<script>var carsData = " . $carsJson . ";</script>";
+        
+         $userRes = Database("Select car_id from view_history where user_id = {$_SESSION['user_id']}", 1, MYSQLI_NUM);
+         $userJson = json_encode($userRes);
+         echo "<script>var userData = " . $userJson . ";</script>";
 
+    ?>
+    </footer>
+
+</body>
+<script>   
+let cars = carsData;
+let car_history_ids = userData;
+
+const updatedCars = new Map();
+
+cars.push([1, "BMW", "SUV", "Black", "38677", "2014"]);
+cars.push([2, "Chevrolet", "SUV", "Yellow", "40649", "2016"]);
+cars.push([3, "BMW", "SUV", "Black", "38677", "2015"]);
+cars.push([4, "Subaru", "Hatchback", "Orange", "25650", "2015"]);
+cars.push([1, "BMW", "SUV", "Black", "38677", "2014"]);
+cars.push([2, "Chevrolet", "SUV", "Yellow", "40649", "2016"]);
+cars.push([3, "BMW", "SUV", "Black", "38677", "2015"]);
+cars.push([4, "Subaru", "Hatchback", "Orange", "25650", "2015"]);
+cars.push([5, "Toyota", "Sedan", "Gray", "39285", "2016"]);
+cars.push([6, "Ford", "Truck", "Silver", "42092", "2011"]);
+cars.push([7, "Chevrolet", "SUV", "Gray", "49168", "2012"]);
+cars.push([8, "Subaru", "SUV", "Blur", "49168", "2012"]);
+cars.push([9, "BMW", "SUV", "Gray", "38677", "2012"]);
+cars.push([10, "BMW", "Sedan", "Red", "42092", "2016"]);
+cars.push([11, "Mercedes-Benz", "Pickup Truck", "Red", "37038", "2015"]);
+cars.push([12, "Mercedes-Benz", "Pickup Truck", "Orange", "37038", "2015"]);
+cars.push([13, "Volvo", "Sports Car", "Brown", "29614", "2012"]);
+cars.push([14, "Nissan", "Coupe", "Yellow", "49168", "2016"]);
+cars.push([15, "Chevrolet", "Hatchback", "Gray", "47867", "2015"]);
+cars.push([16, "Toyota", "Hatchback", "Gray", "47867", "2015"]);
+cars.push([17, "Subaru", "SUV", "Black", "45633", "2016"]);
+
+
+/* Convert the data to numbers */
+const nominalFeatureIndex = [1, 2, 3];
+const numaricFeatureIndex = [4, 5];
+
+const uniqueList = unqList(cars, nominalFeatureIndex);
+
+var numberOfElements = 0;
+
+for (let l of cars) {
+    const key = l[0];
+    const l1 = minMax(l, numaricFeatureIndex);
+    const l2 = OneShotEncoding(l, uniqueList);
+    const fl = l1.concat(l2);
+    updatedCars.set(key, fl);
+}
+
+console.log(updatedCars.get(1).length)
+
+const userVector = new Array(updatedCars.get(1).length).fill(0);
+
+for (let i of car_history_ids) {
+    const l = updatedCars.get(i);
+    for (let z = 0; z < numberOfElements; z++) {
+        userVector[z] += l[z];
+    }
+}
+
+for (let k = 0; k < numberOfElements; k++) {
+    userVector[k] /= car_history_ids.length;
+}
+
+//Calculate similarity
+const finalList = new Map();
+updatedCars.forEach((v, k) => {
+    const similarity = EuclideanSimilarity(userVector, v);
+    finalList.set(similarity, k);
+});
+
+//Sort
+const sortedByKey = new Map(
+    Array.from(finalList).sort((a, b) => a[0] > b[0] ? 1 : -1)
+);
+
+//Print
+console.log(sortedByKey)
+
+function EuclideanSimilarity(vector1, vector2) {
+    let res = 0.0;
+    let sum = 0;
+    for (let i = 0; i < vector1.length; i++) {
+        const diff = vector1[i] - parseFloat(vector2[i]);
+        sum += diff ** 2;
+    }
+
+    res = Math.sqrt(sum);
+    return res;
+}
+
+function unqList(daList, nominalIndexes) {
+    const map = new Map();
+    const res = [];
+    for (let i of nominalIndexes) {
+        for (let l of daList) {
+            map.set(l[i], i);
+        }
+    }
+
+    map.forEach((value, key) => {
+        res.push(key);
+    });
+    numberOfElements = map.size;
+    return res;
+}
+
+function OneShotEncoding(list, uniqueList) {
+    const res = [];
+    for (let l of uniqueList) {
+        if (list.includes(l)) {
+            res.push(1.0);
+        } else {
+            res.push(0.0);
+        }
+    }
+
+    return res;
+}
+
+function minMax(list, numaricIndexes) {
+    const res = [];
+
+    for (const i of numaricIndexes) {
+        let min = Number.MAX_VALUE;
+        let max = Number.MIN_VALUE;
+
+        // find the min and the max
+        for (const row of cars) {
+            const num = parseFloat(row[i]);
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        // Calculation
+        for (let j = 0; j < list.length; j++) {
+            if (i === j) {
+                res.push((parseFloat(list[i]) - min) / (max - min));
+            }
+        }
+    }
+
+    return res;
+}
+
+
+</script>
 </html>
