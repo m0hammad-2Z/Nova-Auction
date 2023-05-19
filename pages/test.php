fb72d195d7932675f8423e2830185fd9718d3f09
Mohammad Al-zaro <imhamd33@gmail.com>
2023-05-19 20:16:18 +0300

    Update test.php

--- pages/test.php
+++ pages/test.php
@@ -5,7 +5,7 @@ require_once "../lib.php";
 $users = Database('Select first_name, id from user_info', 1);
 
 foreach($users as $i){
-    $res = 'https://api.dicebear.com/6.x/initials/png?seed=' . $i[0] . rand(1, 5000);
+    $res = 'https://api.dicebear.com/6.x/adventurer/png?seed=' . $i[0] . rand(1, 5000). "&backgroundRotation=0,360,-310,-320,-330&backgroundColor=ffdfbf,ffd5dc,d1d4f9,c0aede,b6e3f4";
     echo $res;
 Database("UPDATE user_info SET img_path = '$res' where id = $i[1]", 0);
 
